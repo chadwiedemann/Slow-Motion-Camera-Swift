0f27fb5c451ab992feea3424f1074929a81f24fe
Chad Wiedemann <chad.wiedemann@gmail.com>
2016-10-17 10:41:28 -0400

    first commit

---
+++ Slow Motion Camera Swift/VideoPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VideoPlayerViewController.swift
+//  Slow Motion Camera Swift
+//
+//  Created by Chad Wiedemann on 10/17/16.
+//  Copyright © 2016 Chad Wiedemann. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
